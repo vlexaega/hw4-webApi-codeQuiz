03a6f2f517c64f5d93c5defd0339c25c2982e0f5
vlexaega <132007323+vlexaega@users.noreply.github.com>
2023-05-22 17:44:52 -0400

    details saving

--- script.js
+++ script.js
@@ -1,20 +1,52 @@
 // IDEAS OF USE
 // User starts quiz
 var startQuiz = document.querySelector("#startQuiz");
+var questionsAndAnswers = [
+    {
+        question: "What is js short for?",
+        answers: {
+            a: 'Javascript',
+            b: 'Justsurf',
+            c: 'Joinsomething',
+            d: 'justslump',
+        },
+        correctAnswers: 'a',
+    },
+    {
+        question: "What is our professors name?",
+        answers: {
+            a: 'benjanmin',
+            b: 'larry',
+            c: 'json',
+            d: 'billy',
+        }
+    }
+]
 
-// User sees quiz questions and responses 
+// Determining number of questions 
+var questionCount = 0;
 
-// User selects response 
-    // Timer reduces by 10 seconds if response is wrong 
+// Scoreboard details
+var score = 0;
+var secondCount = 120;
+var timeOut = false;
+var endGame = 0;
 
-// User completes quiz
-    // results displayed in prompt 
-//
+// Array to store the results
+var resultsStore = [];
 
 
-// user lands on page, greeted with html landing page
-    // eventListener for click on "start quiz"
-    // timer starts
-// text field updates to question 1
-    // user sees list of response options
-    // 
+
+// ROUGH IDEAS 
+// User sees quiz questions and responses
+    // presented with question 1 and response options
+        // if response incorrect, reduce timer by 10 seconds
+        // else continue
+        // occurs for each question
+            // needs to determine which question user is on
+        // once all questions answered:
+            // display score 
+                // update text to reflect new score and previous score 
+        // prompt user for try again?
+
+
