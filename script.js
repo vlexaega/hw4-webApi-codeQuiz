f7027664b712d6e31d30a4030e693e07f69e461e
vlexaega <132007323+vlexaega@users.noreply.github.com>
2023-05-22 08:05:17 -0400

    new day saves

--- script.js
+++ script.js
@@ -1,2 +1,36 @@
 console.log("hello world");
 console.log("hey vega, i'm working");
+
+var myQuestions = [
+    question1 = "What character is a Logical Not?",
+    answers = {
+        a:'?',
+        b: '%',
+        c: '!',
+    },
+    correctAnswer = 'c'
+]
+console.log(myQuestions);
+
+function showQuestions()
+
+// function showQuestions(questions, quizContainer){
+
+// }
+
+
+
+// IDEAS OF USE
+
+// user lands on page, greeted with html landing page
+//  clicks the button "let's start"
+    // button data replaced by question 1 (load question function)
+    // 
+//  correct or incorrect answer is logged 
+    // if correct, proceed to next question function
+    // if incorrect, timer reduced by 10 seconds 
+        // prompt (Incorrect! 10 seconds deducted)
+        // then proceed to next question function
+//  repeat this step 5 times // define questions 
+// after last question is answered OR timer reaches zero
+    // display score, including number correct and incorrect 
