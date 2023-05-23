95e6cac0f629461738310b24862a101cf671518e
vlexaega <132007323+vlexaega@users.noreply.github.com>
2023-05-22 20:44:31 -0400

    further structuring of JS file

--- script.js
+++ script.js
@@ -1,90 +1,168 @@
-// IDEAS OF USE
-// DEPENDENCIES 
-var questionsEl = document.querySelector('#questions');
-var timerEl = document.querySelector('#timer');
-var startQuizEl = document.querySelector('#startQuiz');
-
-// Determining number of questions 
-var questionCount = 0;
-
-// Scoreboard details
-var score = 0;
-var secondCount = 120;
-var timeOut = false;
-var endGame = 0;
-
-// Array to store the results
-var resultsArray = [];
-// var startQuiz = document.querySelector("#startQuiz");
-var questionsAndAnswers = [
+var questions = [
     {
-        question: "What is js short for?",
-        answers: {
-            a: 'Javascript',
-            b: 'Justsurf',
-            c: 'Joinsomething',
-            d: 'justslump',
-        },
-        correctAnswers: 'a',
-    },
-]
+        question: "What is JS short for?",
+        answers: [
+            { text: "Javascript", correct: true},
+            { text: "Justsurf", correct: false},
+            { text: "Joinsomething", correct: false},
+            { text: "Justslump", correct: false},
+        ]},
+        {
+            question: "What is HTML short for?",
+            answers: [
+                { text: "HyperText Markup Language", correct: true},
+                { text: "Holy Tamale Mile Long", correct: false},
+                { text: "Hey There Mixed Length", correct: false},
+                { text: "Happy To Meet Larry", correct: false},
+            ]},
 
-startQuiz.addEventListener("click", startQuiz);
-// User starts quiz
-// triggers setTime() function
-function startQuiz () {
-    console.log(question);
-    var userSelection = [];
-    if (questionCount > (questionsAndAnswers.length - 1) && (endGameRun <1)) {
-        return endGame();
+];
+var questionEl = document.getElementById("question");
+var answerButtons = document.getElementById("answer-buttons");
+var nextButton = document.getElementById("next-btn");
+
+//store question index and score
+var currentQuestionIndex = 0;
+var score = 0;
+
+// functions
+
+function startQuiz (){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion(){
+    resetState();
+    var currentQuestion = questions[currentQuestionIndex];
+    var questionNo = currentQuestionIndex + 1;
+    questionEl.innerHTML = questionNo + ". " + currentQuestion.question; 
+
+    currentQuestion.answers.forEach(answer => {
+        var button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if (answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
+}
+
+function resetState(){
+    nextButton.style.display = "none";
+    while (answerButtons.firstChild){
+        //remove all previous answers
+        answerButtons.removeChild(answerButtons.firstChild)
+    }
+}
+
+// confirms to user if answer is correct or incorrect
+function selectAnswer(e){
+    var selectedBtn = e.target;
+    var isCorrect = selectedBtn.dataset.correct === "true";
+    if (isCorrect){
+        selectedBtn.classList.add("correct");
     }
     else {
-        var newHeader = document.createElement ("h1");
-        newHeader.textContent = questionsAndAnswers[questionCount].question;
-        newHeader.id = 'questions';
-        document.getElementById("questions").appendChild(newHeader)
+        selectedBtn.classList.add("incorrect");
     }
-
 }
-function startQuiz()
-// Show user the question and listen for clicks on the button #startQuiz
-// function newQuestion(){
-//     question1.addEventLister("click", function() {
+startQuiz();
 
+
+
+
+// // IDEAS OF USE
+// // DEPENDENCIES 
+// var questionsEl = document.querySelector('#questions');
+// var timerEl = document.querySelector('#timer');
+// var startQuizEl = document.querySelector('#startQuiz');
+
+// // Determining number of questions 
+// var questionCount = 0;
+
+// // Scoreboard details
+// var score = 0;
+// var secondCount = 120;
+// var timeOut = false;
+// var endGame = 0;
+
+// // Array to store the results
+// var resultsArray = [];
+// // var startQuiz = document.querySelector("#startQuiz");
+// var questionsAndAnswers = [
+//     {
+//         question: "What is js short for?",
+//         answers: {
+//             a: 'Javascript',
+//             b: 'Justsurf',
+//             c: 'Joinsomething',
+//             d: 'justslump',
+//         },
+//         correctAnswers: 'a',
+//     },
+// ]
+
+// startQuiz.addEventListener("click", startQuiz);
+// // User starts quiz
+// // triggers setTime() function
+// function startQuiz () {
+//     console.log(question);
+//     var userSelection = [];
+//     if (questionCount > (questionsAndAnswers.length - 1) && (endGameRun <1)) {
+//         return endGame();
+//     }
+//     else {
+//         var newHeader = document.createElement ("h1");
+//         newHeader.textContent = questionsAndAnswers[questionCount].question;
+//         newHeader.id = 'questions';
+//         document.getElementById("questions").appendChild(newHeader)
 //     }
 
-// };
+// }
+// function startQuiz()
+// // Show user the question and listen for clicks on the button #startQuiz
+// // function newQuestion(){
+// //     question1.addEventLister("click", function() {
 
-// Creating the timer
-function setTime () {
-    var timerInterval = setInterval(function() {
-        secondCount--;
-        timerEl.textContent = secondCount + " time remaining.";
+// //     }
 
-        if (secondCount === 0) {
-            clearInterval(timerInterval);
-            sendMessage();
-        }
-    }, 1000);
-}
+// // };
 
-function sendMessage () {
-    prompt(questionsAndAnswers);
-}
+// // Creating the timer
+// function setTime () {
+//     var timerInterval = setInterval(function() {
+//         secondCount--;
+//         timerEl.textContent = secondCount + " time remaining.";
 
-// INITIALIZATIONS 
-setTime();
+//         if (secondCount === 0) {
+//             clearInterval(timerInterval);
+//             sendMessage();
+//         }
+//     }, 1000);
+// }
 
-// ROUGH IDEAS 
-// User sees quiz questions and responses
-    // presented with question 1 and response options
-        // if response incorrect, reduce timer by 10 seconds
-        // else continue
-        // occurs for each question
-            // needs to determine which question user is on
-        // once all questions answered:
-            // display score 
-                // update text to reflect new score and previous score 
-        // prompt user for try again?
+// function sendMessage () {
+//     prompt(questionsAndAnswers);
+// }
+
+// // INITIALIZATIONS 
+// setTime();
+
+// // ROUGH IDEAS 
+// // User sees quiz questions and responses
+//     // presented with question 1 and response options
+//         // if response incorrect, reduce timer by 10 seconds
+//         // else continue
+//         // occurs for each question
+//             // needs to determine which question user is on
+//         // once all questions answered:
+//             // display score 
+//                 // update text to reflect new score and previous score 
+//         // prompt user for try again?
 
 
